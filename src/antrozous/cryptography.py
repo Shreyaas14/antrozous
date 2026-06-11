212c52a8d42fd152904fc5bba16a1f0857af4c7a
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2026-06-11 16:48:14 -0400

    function definitions

---
+++ src/antrozous/cryptography.py
@@ -0,0 +1,27 @@
+"""
+Functions needed for:
+- data encryption
+- data decryption
+- keypair signing
+- certificate generation
+"""
+
+from typing import List
+
+def generate_keypair():
+  pass 
+
+def create_and_sign_certificate():
+  pass 
+
+def encrypt():
+  pass 
+
+def decrypt():
+  pass 
+
+def publish_to_relay():
+  pass 
+
+def fetch_from_relay():
+  pass
