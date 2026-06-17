13cb01e5b3c8989a44250a7bb094f5c2e679de18
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2026-06-16 21:33:46 -0400

    keypair creation and signing

--- src/antrozous/cryptography.py
+++ src/antrozous/cryptography.py
@@ -6,13 +6,42 @@ Functions needed for:
 - certificate generation
 """
 
-from typing import List
+import io 
+import json 
+
+from datetime import datetime
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+
 
 def generate_keypair():
-  pass 
+  priv_key = Ed25519PrivateKey.generate()
+  pub_key = priv_key.public_key()
 
-def create_and_sign_certificate():
-  pass 
+  return priv_key, pub_key
+
+def master_keypair():
+  master_priv_key, master_pub_key = generate_keypair()
+  return master_priv_key, master_pub_key
+
+def session_keypair():
+  session_priv_key, session_pub_key = generate_keypair()
+  return session_priv_key, session_pub_key
+
+def create_and_sign_certificate(
+    master_priv_key: Ed25519PrivateKey,
+    master_pub_key: str, 
+    session_pub_key: str):
+  created_at = datetime.now() # might need to switch to utc later
+
+  data = {"master_pubkey": master_pub_key, "session_pubkey": session_pub_key, "created_at": created_at}
+
+  try:
+    b_data = io.BytesIO(json.dumps(data).encode('utf-8'))
+    signed = master_priv_key.sign(b_data.getvalue())
+
+    return signed
+  except Exception as error:
+    print(f"Error occurred while signing certificate: {error}")  
 
 def encrypt():
   pass 
